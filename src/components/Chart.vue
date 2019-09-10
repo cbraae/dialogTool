b5041b34f54194b30dd7b17d2fe0097d8a265f2d
Camilla Olsen <camilla.braae.olsen1@gmail.com>
2019-09-10 18:35:37 +0200

    Basis of calendar not set up

--- src/components/Chart.vue
+++ src/components/Chart.vue
@@ -1,6 +1,8 @@
 
 <template>
-    <div></div>
+    <div id="container">
+        
+    </div>
 </template>
 
 
@@ -27,16 +29,81 @@ export default {
           cleanedData.removeIf( function(item, idx){
               return item == ";";
           })
-
+          drawCalendar();  
          
       },
       deep: true,
       immediate: true
     }
-  },
-  
+  },output() {
+      return this.Chart();
+    }
 }
 
+function drawCalendar(){
+var margin = {top: 40, right: 40, bottom: 40, left: 40},
+    width = 960,
+    height = 500;
+
+var x = d3.scaleTime()
+.domain([new Date(2013, 0, 1), new Date(2014, 0, 1)])
+.range([0, width])
+
+var y = d3.scaleTime()
+.domain([new Date(2013,0,1), new Date(2013, 0,31)])
+.range([0, height])
+
+var xAxis = d3.axisTop()
+.scale(x)
+
+var yAxis = d3.axisLeft()
+.scale(y)
+.ticks(d3.timeDay.every(1))
+.tickFormat(d3.timeFormat('%e'))
+
+ var svg = d3.select("#container").append("svg")
+ .attr("width", width + margin.left + margin.right)
+.attr("height", height + margin.top + margin.bottom)
+ .append("g")
+.attr("transform", "translate(" + margin.left + "," + margin.top + ")");
+
+function makeXaxis() {
+    return d3.axisTop()
+    .scale(x)
+}
+
+function makeYaxis() {
+    return d3.axisLeft()
+    .scale(y)
+    .ticks(d3.timeDay.every(1))
+    .tickFormat(d3.timeFormat('%e'))
+ }
+
+ svg.append("g")
+  .attr("class", "x axis")
+  // .attr("transform", "translate(0," + height + ")")
+  .call(xAxis);
+
+svg.append("g")
+.attr("class", "y axis")
+.attr("transform", "translate(0,0)")
+.call(yAxis);
+
+ svg.append("g")
+ .attr("class", "grid")
+.call(makeYaxis()
+  .tickSize(-width, 0, 0)
+  .tickFormat("")
+  .tickPadding(8)
+  )
+
+svg.append("g")
+.attr("class", "grid")
+.call(makeXaxis()
+  .tickSize(-height, 0, 0)
+  .tickFormat("")
+  )
+}
 
 
 
--- test/src/components/Chart.vue
+++ test/src/components/Chart.vue
@@ -1,6 +1,6 @@
 
 <template>
-    <div></div>
+    <div id="container"></div>
 </template>
 
 
@@ -22,20 +22,94 @@ export default {
   watch: {
   	trackingData: {
       handler: function() {
-          
-          var cleanedData = this.trackingData.map(item => (item[this.trackingData.columns[0]]));
+          console.log(this.trackingData)
+          /*var cleanedData = this.trackingData.map(item => (item[this.trackingData.columns[0]]));
           cleanedData.removeIf( function(item, idx){
               return item == ";";
-          })
-
+          })*/
+            console.log(cleanedData)
          
       },
       deep: true,
       immediate: true
     }
-  },
-  
+  },output() {
+      return this.Chart();
+    }
 }
+
+var margin = {top: 40, right: 40, bottom: 40, left: 40},
+    width = 960,
+    height = 500;
+
+var x = d3.time.scale()
+.domain([new Date(2013, 0, 1), new Date(2014, 0, 1)])
+.range([0, width])
+
+var y = d3.time.scale()
+.domain([new Date(2013,0,1), new Date(2013, 0,31)])
+.range([0, height])
+
+var xAxis = d3.svg.axis()
+.scale(x)
+.orient("top")
+
+var yAxis = d3.svg.axis()
+.scale(y)
+.orient("left")
+.ticks(d3.time.days, 1)
+.tickFormat(d3.time.format('%e'))
+
+ var svg = d3.select(".container").append("svg")
+ .attr("width", width + margin.left + margin.right)
+.attr("height", height + margin.top + margin.bottom)
+ .append("g")
+.attr("transform", "translate(" + margin.left + "," + margin.top + ")");
+
+function makeXaxis() {
+return d3.svg.axis()
+.scale(x)
+.orient("top")
+}
+
+function makeYaxis() {
+return d3.svg.axis()
+.scale(y)
+.orient("left")
+.ticks(d3.time.days, 1)
+.tickFormat(d3.time.format('%e'))
+ }
+
+ svg.append("g")
+  .attr("class", "x axis")
+  // .attr("transform", "translate(0," + height + ")")
+  .call(xAxis);
+
+svg.append("g")
+.attr("class", "y axis")
+.attr("transform", "translate(0,0)")
+.call(yAxis);
+
+ svg.append("g")
+ .attr("class", "grid")
+.call(makeYaxis()
+  .tickSize(-width, 0, 0)
+  .tickFormat("")
+  .tickPadding(8)
+  )
+
+svg.append("g")
+.attr("class", "grid")
+.call(makeXaxis()
+  .tickSize(-height, 0, 0)
+  .tickFormat("")
+  )
+
+
+
+
+
+
 
 
 
